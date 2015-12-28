f8865543758b0816cd54457b480b64762f978d81
Jacek <jacek@opcode.pl>
2015-12-28 23:16:11 +0100

    Initial commit

---
+++ src/EstimatedDeliveryDate.php
@@ -0,0 +1,96 @@
+<?php
+
+class EstimatedDeliveryDate
+{
+    /** @var  DateTimeImmutable $currentDate */
+    private $currentDate = 'now';
+    private $deliveryMin = 0;
+    private $deliveryMax = 0;
+    private $preparation = 0;
+    private $vacations = [];
+    private $preparationDaysOfWeek = [1,2,3,4,5]; //1 is Monday, 5 is Friday
+    private $deliveryDaysOfWeek = [1,2,3,4,5]; //1 is Monday, 5 is Friday
+
+    public function __construct($currentDate)
+    {
+        $date = new DateTimeImmutable();
+        $this->currentDate = $date->createFromFormat('Y-m-d', $currentDate, new DateTimeZone('UTC'));
+    }
+
+    public function delivery($min, $max)
+    {
+        $this->deliveryMin = $min;
+        $this->deliveryMax = $max;
+    }
+
+    public function preparation($days)
+    {
+        $this->preparation = $days;
+    }
+
+    public function vacations(array $vacations)
+    {
+        $this->vacations = $vacations;
+    }
+
+    public function estimation()
+    {
+        $preparationDate = $this->prepare($this->currentDate, $this->preparation);
+        $daysMin = $this->deliver($this->currentDate, $this->deliveryMin) + $preparationDate;
+        $daysMax = $this->deliver($this->currentDate, $this->deliveryMax) + $preparationDate;
+
+
+        $min = $this->currentDate->modify('+' . $daysMin . 'days');
+        $max = $this->currentDate->modify('+' . $daysMax . 'days');
+
+        return [
+            'min' => $min->format('Y-m-d'),
+            'max' => $max->format('Y-m-d'),
+        ];
+    }
+
+    private function prepare(DateTimeImmutable $date, $preparation)
+    {
+        $workingDays = 0;
+
+        for($i=0;; $i++) {
+            $calendarDay = $date->modify('+' . $i . ' days');
+
+            if(in_array($calendarDay->format('Y-m-d'), $this->vacations)) {
+                continue;
+            } elseif(!in_array($calendarDay->format('N'), $this->preparationDaysOfWeek)) {
+                continue;
+            }
+
+            $workingDays++;
+
+            if($workingDays > $preparation) {
+                break;
+            }
+        }
+
+        return $i;
+    }
+
+    private function deliver(DateTimeImmutable $date, $delivery)
+    {
+        $workingDays = 0;
+
+        for($i=0;; $i++) {
+            $calendarDay = $date->modify('+' . $i . ' days');
+
+
+            if(!in_array($calendarDay->format('N'), $this->deliveryDaysOfWeek)) {
+                continue;
+            }
+
+            $workingDays++;
+
+            if($workingDays > $delivery) {
+                break;
+            }
+        }
+
+        return $i;
+    }
+}
---
+++ tests/EstimatedDeliveryDateTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\EstimatedDeliveryDate;
+
+use EstimatedDeliveryDate;
+
+class EstimatedDeliveryDateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPreparation()
+    {
+        $currentDate = "2015-12-28";
+        $estimatedDeliveryDate = new EstimatedDeliveryDate($currentDate);
+        $estimatedDeliveryDate->preparation(2);
+        $estimatedDeliveryDate->vacations(['2015-12-29']);
+
+        $deliveryDate = [
+            'min' => "2015-12-31",
+            'max' => "2015-12-31"
+        ];
+
+        $this->assertEquals($deliveryDate, $estimatedDeliveryDate->estimation());
+    }
+
+    public function testDelivery()
+    {
+        $currentDate = "2015-12-28";
+        $estimatedDeliveryDate = new EstimatedDeliveryDate($currentDate);
+        $estimatedDeliveryDate->delivery(0, 1);
+
+        $deliveryDate = [
+            'min' => "2015-12-28",
+            'max' => "2015-12-29"
+        ];
+
+        $this->assertEquals($deliveryDate, $estimatedDeliveryDate->estimation());
+    }
+
+    public function testDeliveryAndPreparation()
+    {
+        $currentDate = "2015-12-28";
+        $estimatedDeliveryDate = new EstimatedDeliveryDate($currentDate);
+        $estimatedDeliveryDate->delivery(0, 1);
+        $estimatedDeliveryDate->preparation(2);
+
+        $deliveryDate = [
+            'min' => "2015-12-30",
+            'max' => "2015-12-31"
+        ];
+
+        $this->assertEquals($deliveryDate, $estimatedDeliveryDate->estimation());
+    }
+
+    public function testDeliveryAndPreparationWithDaysOff()
+    {
+        $currentDate = "2016-01-04";
+        $estimatedDeliveryDate = new EstimatedDeliveryDate($currentDate);
+        $estimatedDeliveryDate->delivery(0, 1);
+        $estimatedDeliveryDate->preparation(5);
+        $estimatedDeliveryDate->vacations(['2016-01-06']);
+
+        $deliveryDate = [
+            'min' => "2016-01-12",
+            'max' => "2016-01-13"
+        ];
+
+        $this->assertEquals($deliveryDate, $estimatedDeliveryDate->estimation());
+    }
+}
